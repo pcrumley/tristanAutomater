1287e50caf41c0c8e2129273566b3cf9f6e0fa1f
Patrick Crumley <patrick.crumley@gmail.com>
2019-05-03 16:06:46 -0400

    removed yaml from examples

--- examples.py
+++ examples.py
@@ -1,27 +1,17 @@
 from tristanSim  import TristanSim
-import yaml, os
+import os
 import matplotlib.pyplot as plt
 import numpy as np
 
-yamlFile='config.yaml'
-with open(yamlFile, 'r') as stream:
-    try:
-        config = yaml.safe_load(stream)
-        #print(config)
-    except yaml.YAMLError as exc:
-        print(exc)
-        
-#outdir = config['ROOT_DIRECTORY']
-
 outdir = '../batchTristan'
 runs = []
-# runs will be a list a tristan simulations instances 
+# runs will be a list a tristan simulations instances
 # that reside in outdir
 
 
 # So any run can be accessed as run4 = runs[4]
 # to access the output time e.g. 4th output, run4Time3 = runs[4][3]
-# NOTE: because runs is a 1D list of objects, treating 
+# NOTE: because runs is a 1D list of objects, treating
 # it as a 2D list, e.g. runs[4,3] WILL NOT WORK.
 
 runNames = [] #the directory name that automater created.
@@ -34,7 +24,7 @@ for elm in os.listdir(outdir):
             runNames.append(os.path.split(os.path.split(elm)[0])[-1])
 
 # TristanSim is an object that exposes an API to access tristan.
-# The cool thing is nothing is loaded until it is accessed for 
+# The cool thing is nothing is loaded until it is accessed for
 # first time, then it is cached.
 
 # fields are accessed via sim.output[n].ex [ex, ey, ez, bx, by, bz, dens...]
@@ -95,10 +85,10 @@ plt.show()
 #
 ###
 
-# you'll find all of the tracked particles in a trackedLecs and 
+# you'll find all of the tracked particles in a trackedLecs and
 # trackedIon object. The first call builds the database which may take
 # awhile. It is saved afterwards. Let's plot a single tracked electron
-# for these runs we didn't track the ions. 
+# for these runs we didn't track the ions.
 
 # Let's focus just on one run for simplicity
 
@@ -108,7 +98,7 @@ myRun = runs[0]
 choice = np.random.randint(len(myRun.trackedLecs))
 randPrtl = myRun.trackedLecs[choice]
 
-# Each prtl has the following attributes: 'x', 'y', 'u', 'v', 'w', 
+# Each prtl has the following attributes: 'x', 'y', 'u', 'v', 'w',
 # 'gamma', 'bx', 'by', 'bz', 'ex', 'ey', 'ez'
 plt.plot(randPrtl.t, randPrtl.gamma)
 plt.show()
