431b4eca004143798dfd9c0411fd725986e789ea
Patrick Crumley <patrick.crumley@gmail.com>
2019-05-02 21:35:29 -0400

    shortening the api

--- tristanSim.py
+++ tristanSim.py
@@ -24,11 +24,26 @@ class TristanSim(object):
         self._outputFileKey = [key.split('.')[0] for key in self._outputFileNames]
         self._outputFileRegEx = [re.compile(elm) for elm in self._outputFileNames]
         self._pathDict = {}
+        self._collisionFixers = {'time': 'param', 'dens': 'flds'}
         self.dir = str(dirpath)
         self.getFileNums()
         self.xtraStride = xtraStride
-        self.output = [OutputPoint(self, n=x) for x in self.getFileNums()]
+        self._h5Key2FileDict = {}
+        self.__output = [OutputPoint(self, n=x) for x in self.getFileNums()]
 
+
+        if len(self) != 0:
+            # Build an key to h5 file dictionary
+            for fkey in self._outputFileKey:
+                for key in getattr(self[0], '_'+fkey).keys():
+                    if key in self._h5Key2FileDict.keys():
+                        if key in self._collisionFixers.keys():
+                            self._h5Key2FileDict[key] = self._collisionFixers[key]
+                            print(f'{key} in {fkey} has collision with {self._collisionFixers[key]}')
+                            print(f' you can find {key} in sim[i]._{fkey}.{key}')
+                    else:
+                        self._h5Key2FileDict[key] = fkey
+            self.__output[0].setMapper(self._h5Key2FileDict)
     def getFileNums(self):
         try:
             # Create a dictionary of all the paths to the files
@@ -38,7 +53,7 @@ class TristanSim(object):
                 self._pathDict[key].sort()
                 for i in range(len(self._pathDict[key])):
                     elm = self._pathDict[key][i]
-                    try: 
+                    try:
                         int(elm.split('.')[-1])
                     except ValueError:
                         if elm.split('.')[-1] == '***':
@@ -62,27 +77,62 @@ class TristanSim(object):
     @cachedProperty
     def trackedIons(self):
         return TrackedDatabase(self, 'ions', keys = self._trackKeys)
+    def __len__(self):
+        #return np.sum(self._mask)
+        return len(self.__output)
 
+    def __getitem__(self, val):
+        return self.__output[val]
+    def loadAllFields(self):
+        for out in self:
+            for key, val in self._h5Key2FileDict.items():
+                if val == 'flds':
+                    getattr(out, key)
+    def loadAllPrtl(self):
+        for out in self:
+            for key, val in self._h5Key2FileDict.items():
+                if val == 'prtl':
+                    getattr(out, key)
+class ObjectMapper(object):
+    '''A base object that holds the info of one type of particle in the simulation
+    '''
+    __h5_dict = {}
+    def __init__(self, sim, n=0):
+        pass
+    @classmethod
+    def setMapper(cls, dict):
+        cls.__h5_dict = {key: val for key, val in dict.items()}
+    @classmethod
+    def getMapper(cls):
+        return cls.__h5_dict
 
-class OutputPoint(object):
+class OutputPoint(ObjectMapper):
     '''A object that provides an API to access data from Tristan-mp
     particle-in-cell simulations. The specifics of your simulation should be
     defined as a class that extends this object.'''
     def __init__(self, sim, n=0):
+        self._sim = sim
         self.__myKeys = []
+
+
         for key, fname in zip(sim._outputFileKey, sim._outputFileNames):
             self.__myKeys.append(key)
             tmpStr = ''
             for elm in fname.split('.')[:-1]:
                 tmpStr += elm +'.'
             tmpStr += n
-            setattr(self, key, h5Wrapper(os.path.join(sim.dir, tmpStr)))
+            setattr(self, '_'+key, h5Wrapper(os.path.join(sim.dir, tmpStr)))
+    def __getattribute__(self, name):
+        if name in super().getMapper():
+            return getattr(getattr(self,'_'+self._sim._h5Key2FileDict[name]), name)
+        else:
+            return object.__getattribute__(self, name)
 
     def reload(self):
         for key in self.__myKeys:
             getattr(self, key).reload()
 
-    
+
 class h5Wrapper(object):
     def __init__(self, fname):
         self._fname = fname
@@ -91,7 +141,7 @@ class h5Wrapper(object):
     def __getattribute__(self, name):
         if object.__getattribute__(self, name) is None:
             if name in self.__h5keys:
-                with h5py.File(self._fname, 'r') as f:                
+                with h5py.File(self._fname, 'r') as f:
                     setattr(self, name, f[name][:])
         return object.__getattribute__(self, name)
 
@@ -109,6 +159,5 @@ if __name__=='__main__':
     import time
     import matplotlib.pyplot as plt
     mySim = TristanSim('../Iseult/output')
-    plt.imshow(mySim.output[0].flds.ex[0,:,:])
-    print(mySim.output[0].flds.keys())
+    plt.imshow(mySim[0].ex[0,:,:])
     plt.show()
